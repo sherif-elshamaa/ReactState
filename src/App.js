a398401588b120984e0aff5b2717e56b49e6b87d
sherif <sherif.elshamaaa@gmail.com>
2021-11-16 14:44:59 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -8,26 +8,29 @@ class App extends React.Component {
     imgSrc: img,
     profession: "fullstack developer",
     show: true,
-    date: new Date(),
+    counter:0,
   };
 
   handletoggle = (e) => {
     e.preventDefault();
-    this.setState({ show: !this.state.show });
+    this.setState({ show: !this.state.show,counter:0 });
+    
   };
   componentDidMount() {
-    this.timerID = setTimeout(() => this.tick(),0);
+    this.timerID = setInterval(() => this.tick(),1000);
   }
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
   tick() {
-    this.setState({date: new Date()});
+    this.setState({counter: this.state.counter+1});
   }
+
+
   render() {
     return (
       <div className="App" style={{ textAlign: "center" }}>
-        <h2>{this.state.date.toLocaleTimeString()}</h2>
+        <h2>{this.state.counter} m/s</h2>
         <button
           onClick={this.handletoggle}
           style={{ width: "100px", height: "50px", backgroundColor: "red" }}
